012a2062e8fa73650bed82b4bf0040aecca65142
AleksanderOrlicz <aleksander.orlicz@gmail.com>
2023-03-16 14:42:23 +0100

    zadanie dzień 4 poprawa

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -7,32 +7,32 @@
 // (2) "Ewa, lat 33"
 // (3) "Niepełnoletni Mężczyzna"
 
-string komunikat1 = "Kobieta poniżej 30 lat";
-string komunikat2 = "Ewa, lat 33";
-string komunikat3 = "Niepełnoletni Mężczyzna";
+string comment1 = "Kobieta poniżej 30 lat";
+string comment2 = "Ewa, lat 33";
+string comment3 = "Niepełnoletni Mężczyzna";
 
 
-var name = "Jan";
-bool czyKobieta = false;    // założenie istnienia dwóch płci ;)
+var name = "Anna";
+bool ifWoman = true;    // założenie istnienia dwóch płci ;)
 string gender;
-int age = 25;
+int age = 35;
 
 
-if (czyKobieta == true && age < 30)
+if (ifWoman == true && age < 30)
 {
-    Console.WriteLine(komunikat1);
+    Console.WriteLine(comment1);
 }
 else if (name == "Ewa" && age == 33)
 {
-    Console.WriteLine(komunikat2);
+    Console.WriteLine(comment2);
 }
-else if (czyKobieta != true && age < 18)
+else if (ifWoman != true && age < 18)
 {
-    Console.WriteLine(komunikat3);
+    Console.WriteLine(comment3);
 }
 else
 {
-    if (czyKobieta == true)
+    if (ifWoman == true)
     {
         gender = "Kobieta";
     }
