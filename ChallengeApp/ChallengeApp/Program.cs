4919e77fcf5946c6a64d7ffef93ec723a997670f
AleksanderOrlicz <aleksander.orlicz@gmail.com>
2023-03-16 13:29:04 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//-zadeklaruj zmienną z imieniem
+//- zadeklaruj zmienną z płcią (dobierz zmienną
+//-zadeklaruj zmienną z wiekiem
+//=zadeklaruj dane i wyświetlasz
+//jeden z komunikatów:
+// (1) "Kobieta poniżej 30 lat"
+// (2) "Ewa, lat 33"
+// (3) "Niepełnoletni Mężczyzna"
+
+string komunikat1 = "Kobieta poniżej 30 lat";
+string komunikat2 = "Ewa, lat 33";
+string komunikat3 = "Niepełnoletni Mężczyzna";
+
+
+var name = "Jan";
+bool czyKobieta = false;    // założenie istnienia dwóch płci ;)
+string gender;
+int age = 25;
+
+
+if (czyKobieta == true && age < 30)
+{
+    Console.WriteLine(komunikat1);
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine(komunikat2);
+}
+else if (czyKobieta != true && age < 18)
+{
+    Console.WriteLine(komunikat3);
+}
+else
+{
+    if (czyKobieta == true)
+    {
+        gender = "Kobieta";
+    }
+    else
+    {
+        gender = "Mężczyzna";
+    }
+    Console.WriteLine(name + " " + gender + " " + age + " lat");
+}
