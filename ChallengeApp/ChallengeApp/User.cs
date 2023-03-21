6fa1d7763654ec6f68d1246dc13cf93118f74e23
AleksanderOrlicz <aleksander.orlicz@gmail.com>
2023-03-21 17:10:09 +0100

    User z dnia 6

---
+++ ChallengeApp/ChallengeApp/User.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ChallengeApp
+{
+    public class User
+    {
+        private List<int> score = new List<int>();
+        public User(string name, string password)
+        {
+            this.Login = name;
+            this.Password = password;
+        }
+
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number)
+        {
+            score.Add(number);
+        }
+    }
+}
