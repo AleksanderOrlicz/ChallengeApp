11f510addaf2994dae443c23d4179b4ca1259181
AleksanderOrlicz <aleksander.orlicz@gmail.com>
2023-04-04 00:52:54 +0200

    Aktualny stan przed dniem 12

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -4,20 +4,12 @@ var employee = new Employee("Aleksander", "Orlicz");
 employee.AddGrade("Adam");
 employee.AddGrade("4000");
 employee.AddGrade(6);
-employee.AddGrade(long.MaxValue);
-employee.AddGrade(double.MaxValue);
-employee.AddGrade(6.55555555555555555555f);
 employee.AddGrade(2);
+employee.AddGrade(6);
+
 var statistics = employee.GetStatistics();
-var statistics2 = employee.GetStatisticsWithDoWhile();
-var statistics3 = employee.GetStatisticsWithWhile();
-var statistics4 = employee.GetStatisticsWithForEach();
-var statistics5 = employee.GetStatisticsWithFor();
 
 
 
 
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Min: {statistics.Min}");
-Console.WriteLine($"Max: {statistics.Max}");
 
